47c2c0dde346aa25285df355a36a01c5bfd48a96
silvodesigns <kevsilvo@gmail.com>
2019-10-10 17:25:10 -0400

    converted to class

--- user-card/src/App.js
+++ user-card/src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-   
-    </div>
-  );
-}
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state = null
+  }
+
+  render(){
+    return (
+      <div className="user-card">
+     
+      </div>
+    );
+
+  }
+} 
+ 
+
 
 export default App;
