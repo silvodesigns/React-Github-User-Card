68aec1a35a541f3f987824e6884ff62cf67f34f6
silvodesigns <kevsilvo@gmail.com>
2019-10-10 23:42:19 -0400

    reading from state

--- user-card/src/App.js
+++ user-card/src/App.js
@@ -1,16 +1,44 @@
 import React from 'react';
 import './App.css';
+import axios from 'axios';
 
 class App extends React.Component{
   constructor(){
     super();
-    this.state = null
+    this.state = {
+      user: null
+    }
+  }
+
+  componentDidMount() {
+    axios.get('https://api.github.com/search/users?q=silvodesigns')
+      .then( res => { 
+        this.setState({user: res.data.items[0]})
+        console.log(this.state.user);
+      })
+     .catch( err => {
+       console.log(err);
+      
+     })
+
+     
+
   }
 
   render(){
+
+    let information;
+
+    if(this.state.user !== null){
+      information = <div>
+        <h1>Username: {this.state.user.login}</h1>
+      </div>
+
+    } 
+
     return (
       <div className="user-card">
-     
+          {information}
       </div>
     );
 
